78d3acd5acae57b6dfbf44b777ade2184a86238f
unknown <njerivictory52@gmail.com>
2024-08-04 15:55:28 +0300

    create pages

---
+++ client/src/pages/Contactus/Contactus.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Contactus.css'
+
+function Contactus() {
+  return (
+    <div>Contactus</div>
+  )
+}
+
+export default Contactus
---
+++ client/src/pages/Wishlist/Wishlist.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Wishlist.css'
+
+function Wishlist() {
+  return (
+    <div>Wishlist</div>
+  )
+}
+
+export default Wishlist
