be1f64a2c5fa68326ab04fa467f53ae695e30f3c
unknown <njerivictory52@gmail.com>
2024-08-06 18:41:33 +0300

    Admin page Nav

--- client/Components/AdminNav/Admin.jsx
+++ client/Components/AdminNav/Admin.jsx
@@ -8,13 +8,19 @@ function Admin() {
   return (
    <>
       <div className='AdminNav'>
+       
       <NavLink to="/AdminHome" className={({ isActive }) => (isActive ? 'active' : '')}>Home</NavLink>
       <NavLink to="/AdminUsers" className={({ isActive }) => (isActive ? 'active' : '')}>AdminUsers</NavLink>     
         <NavLink to="/AddAdmin" className={({ isActive }) => (isActive ? 'active' : '')}>AddAdmin</NavLink>
         <NavLink to="/PostProduct" className={({ isActive }) => (isActive ? 'active' : '')}>PostProduct</NavLink>
       </div>
 
-
+      <p>b</p>
+      <p>b</p>
+      <p>b</p>
+      <p>b</p>
+      <p>b</p>
+      <p>b</p>
           </>
       
   
